0aa4f8f3773cfa25c502ed1178d1203ea4f4cb25
Aндрей Бушуев <B.A.A.2002@yandex.ru>
2022-03-14 23:51:22 +0300

    Создано масштабирование таблицы истинности

--- Logic/Form1.Designer.cs
+++ Logic/Form1.Designer.cs
@@ -29,28 +29,16 @@ namespace Logic
         /// </summary>
         private void InitializeComponent()
         {
+            this.variable_comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.true_table = new System.Windows.Forms.TableLayoutPanel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // true_table
+            // variable_comboBox
             // 
-            this.true_table.ColumnCount = 2;
-            this.true_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.true_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.true_table.Location = new System.Drawing.Point(13, 42);
-            this.true_table.Name = "true_table";
-            this.true_table.RowCount = 2;
-            this.true_table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.true_table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.true_table.Size = new System.Drawing.Size(200, 268);
-            this.true_table.TabIndex = 0;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.variable_comboBox.FormattingEnabled = true;
+            this.variable_comboBox.Items.AddRange(new object[] {
             "1",
             "2",
             "3",
@@ -58,27 +46,64 @@ namespace Logic
             "5",
             "6",
             "7"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 1;
+            this.variable_comboBox.Location = new System.Drawing.Point(15, 36);
+            this.variable_comboBox.Name = "variable_comboBox";
+            this.variable_comboBox.Size = new System.Drawing.Size(121, 24);
+            this.variable_comboBox.TabIndex = 1;
+            this.variable_comboBox.SelectedIndexChanged += new System.EventHandler(this.variable_comboBox_SelectedIndexChanged);
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 335);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(201, 149);
-            this.textBox1.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество переменных";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Таблица истинности";
+            // 
+            // true_table
+            // 
+            this.true_table.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.true_table.AutoSize = true;
+            this.true_table.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.true_table.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.true_table.ColumnCount = 2;
+            this.true_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.true_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.true_table.Location = new System.Drawing.Point(16, 113);
+            this.true_table.Name = "true_table";
+            this.true_table.RowCount = 2;
+            this.true_table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.true_table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.true_table.Size = new System.Drawing.Size(453, 3);
+            this.true_table.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(225, 496);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(482, 131);
             this.Controls.Add(this.true_table);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.variable_comboBox);
+            this.MaximumSize = new System.Drawing.Size(500, 600);
             this.Name = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -86,10 +111,10 @@ namespace Logic
         }
 
         #endregion
-
+        private System.Windows.Forms.ComboBox variable_comboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TableLayoutPanel true_table;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Logic/Form1.cs
+++ Logic/Form1.cs
@@ -15,6 +15,54 @@ namespace Logic
         public Form1()
         {
             InitializeComponent();
+            variable_comboBox.SelectedIndex = 0;
         }
+
+
+        private void variable_comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            true_table.Controls.Clear();
+            int var_count = Convert.ToInt32(variable_comboBox.SelectedItem);
+            true_table.RowCount = (int)Math.Pow(2, var_count) + 1;
+            //Создание подписи
+            int a = 97;//ACII
+            string fn_text = "";
+            string var_text = "";
+            for (int i = a; i< a + var_count; i++)
+            {
+                fn_text += Convert.ToChar(i);
+                var_text += Convert.ToChar(i);
+                if (i != a + var_count - 1) fn_text += ",";
+            }
+            Font top_font = new Font("Microsoft Sans Serif", 10f, FontStyle.Bold);
+            Label fn_lable = new Label();
+            fn_lable.Text = "F(" + fn_text + ")";
+            fn_lable.Font  = top_font;
+            fn_lable.AutoSize = true;
+            true_table.Controls.Add(fn_lable, 1, 0);
+
+            Label var_lable = new Label();
+            var_lable.Text = var_text;
+            var_lable.Font = top_font;
+            true_table.Controls.Add(var_lable, 0, 0);
+
+            //Заполнение строк
+            Label row_lable = new Label();
+            Font main_font = new Font("Microsoft Sans Serif", 10f);
+            for (int i = 0; i < Math.Pow(2, var_count); i++)
+            {
+                row_lable = new Label();
+                row_lable.Font = main_font;
+                string row_text = Convert.ToString(i, 2);
+                row_lable.Text = string.Concat(Enumerable.Repeat("0", var_count-row_text.Length))+row_text;
+                row_lable.AutoSize = true;
+                true_table.Controls.Add(row_lable, 0, i+1);
+            }
+             
+            foreach (ColumnStyle st in true_table.ColumnStyles) st.SizeType = SizeType.AutoSize;
+            foreach (RowStyle rs in true_table.RowStyles) rs.SizeType = SizeType.AutoSize;
+        }
+
     }
 }
