7a0c1c60f8fe03a46a98a605bbe2661a658f7cce
Aндрей Бушуев <B.A.A.2002@yandex.ru>
2022-03-16 16:48:52 +0300

    Создан класс LogiEq

--- Logic/Form1.cs
+++ Logic/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Logic
@@ -22,26 +17,30 @@ namespace Logic
 
         private CheckBox[] checkBoxes;
         private Result f;
+        private int var_count;
+        private string[] var_names;
         private void variable_comboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
 
             true_table.Controls.Clear();
-            int var_count = Convert.ToInt32(variable_comboBox.SelectedItem);
+            var_count = Convert.ToInt32(variable_comboBox.SelectedItem);
             true_table.RowCount = (int)Math.Pow(2, var_count) + 1;
             //Создание подписи
             int a = 97;//ACII
             string fn_text = "";
             string var_text = "";
-            for (int i = a; i< a + var_count; i++)
+            var_names = new string[var_count];
+            for (int i = a; i < a + var_count; i++)
             {
                 fn_text += Convert.ToChar(i);
                 var_text += Convert.ToChar(i);
+                var_names[i - a] = Convert.ToString(Convert.ToChar(i));
                 if (i != a + var_count - 1) fn_text += ",";
             }
             Font top_font = new Font("Microsoft Sans Serif", 10f, FontStyle.Bold);
             Label fn_lable = new Label();
             fn_lable.Text = "F(" + fn_text + ")";
-            fn_lable.Font  = top_font;
+            fn_lable.Font = top_font;
             fn_lable.AutoSize = true;
             true_table.Controls.Add(fn_lable, 1, 0);
 
@@ -53,15 +52,15 @@ namespace Logic
             //Заполнение строк
             Label row_lable = new Label();
             Font main_font = new Font("Microsoft Sans Serif", 10f);
-            CheckBox[] checkBoxes = new CheckBox[(int)Math.Pow(2, var_count)]; 
+            checkBoxes = new CheckBox[(int)Math.Pow(2, var_count)];
             for (int i = 0; i < Math.Pow(2, var_count); i++)
             {
                 row_lable = new Label();
                 row_lable.Font = main_font;
                 string row_text = Convert.ToString(i, 2);
-                row_lable.Text = string.Concat(Enumerable.Repeat("0", var_count-row_text.Length))+row_text;
+                row_lable.Text = string.Concat(Enumerable.Repeat("0", var_count - row_text.Length)) + row_text;
                 row_lable.AutoSize = true;
-                true_table.Controls.Add(row_lable, 0, i+1);
+                true_table.Controls.Add(row_lable, 0, i + 1);
 
                 checkBoxes[i] = new CheckBox();
                 checkBoxes[i].Text = "";
@@ -74,7 +73,10 @@ namespace Logic
 
         private void FValueChanged(object sender, EventArgs e)
         {
-
+            bool[] FValues = new bool[(int)Math.Pow(2, var_count)];
+            for (int i = 0; i < (int)Math.Pow(2, var_count); i++) FValues[i] = checkBoxes[i].Checked;
+            LogicEq eq = new LogicEq(FValues, var_names);
+            eq.ToPolinom();
         }
     }
 }
---
+++ Logic/LogicEq.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Logic
+{
+    public class LogicEq
+    {
+        private string or = "⋁";
+        private string and = "⋀";
+        private string not = "¬";
+        private string xor = "⊕";
+
+        private bool[] values;
+        private string[] var_names;
+        public bool[] Values => values;
+        public LogicEq(bool[] val, string[] var_n)
+        {
+            if (var_n.Length != (int)Math.Log(val.Length, 2)) throw new Exception("Wrong number of variables");
+            values = val;
+            var_names = var_n;
+        }
+        public string ToPDF() //СДНФ
+        {
+            string result = "";
+            return result;
+        }
+        public string ToPolinom()//Полином Жегалкина
+        {
+            string result = "";
+            bool[] r = create_var_val(0);
+            r = create_var_val(var_names.Length-1);
+            return result;
+        }
+        private bool[] create_var_val(int n)//Строка таблицы истинности
+        {
+            bool[] result = new bool[var_names.Length];
+            int i = result.Length - 1;
+            while (n != 0)
+            {
+                if (n % 2 == 0) result[i] = false;
+                else result[i] = true;
+                i--;
+                n /= 2;
+            }
+            return result;
+        }
+    }
+}
